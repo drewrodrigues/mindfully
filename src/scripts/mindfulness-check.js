d3c66aa0ff759acb271e0cd416d5a8ab1766b5d2
drew <drew@drews-MacBook-Pro.local>
2023-05-19 15:55:56 -0700

    {WIP} Redirecting reddit to mindfully html page

---
+++ src/scripts/mindfulness-check.js
@@ -0,0 +1,15 @@
+let html = chrome.runtime.getURL("src/views/mindfulness-check.html");
+
+// window.location.href = html;
+// console.log(html);
+// console.log("mindfulness-check.js");
+
+// const iFrame = document.createElement("iframe");
+// iFrame.src = html;
+// iFrame.style.left = "0px";
+// iFrame.style.right = "0px";
+// iFrame.style.bottom = "0px";
+// iFrame.style.top = "0px";
+// iFrame.style.position = "fixed";
+
+// document.body.insertBefore(iFrame, document.body.firstChild);
---
+++ src/scripts/serviceWorker.js
@@ -0,0 +1,9 @@
+console.log("Service worker registered");
+
+// chrome.webRequest.onBeforeRequest.addListener(
+//   (details) => {
+//     console.log("onBeforeRequest", details);
+//   },
+//   { urls: ["<all_urls>"] },
+//   ["blocking"]
+// );
