3cd953388e117160df23f57cea255fe0595feef7
drew <drew@drews-MacBook-Pro.local>
2023-05-23 18:10:43 -0700

    Basic JS add functionality

--- src/scripts/serviceWorker.js
+++ src/scripts/serviceWorker.js
@@ -3,37 +3,32 @@ console.log('Service worker registered')
   try {
     // const staticRuleCount = await chrome.declarativeNetRequest.getSessionRules()
     // console.log(staticRuleCount)
-
     // const enabledRules = await chrome.declarativeNetRequest.getEnabledRulesets()
     // console.log(enabledRules)
-
     // TODO: sync these with a settins/config page
-
-    console.log('Checking dynamic rules')
-    let dynamicRuleCount = await chrome.declarativeNetRequest.getDynamicRules()
-    console.log(dynamicRuleCount)
-
-    console.log('Setting dynamic rule')
-    await chrome.declarativeNetRequest.updateDynamicRules({
-      addRules: [
-        {
-          id: 10002,
-          priority: 1,
-          action: {
-            type: 'redirect',
-            redirect: { extensionPath: '/src/views/mindfulness-check.html' },
-          },
-          condition: {
-            urlFilter: 'peanuts',
-            resourceTypes: ['main_frame'],
-          },
-        },
-      ],
-    })
-
-    console.log('Checking dynamic rule count')
-    dynamicRuleCount = await chrome.declarativeNetRequest.getDynamicRules()
-    console.log(dynamicRuleCount)
+    // console.log('Checking dynamic rules')
+    // let dynamicRuleCount = await chrome.declarativeNetRequest.getDynamicRules()
+    // console.log(dynamicRuleCount)
+    // console.log('Setting dynamic rule')
+    // await chrome.declarativeNetRequest.updateDynamicRules({
+    //   addRules: [
+    //     {
+    //       id: 10002,
+    //       priority: 1,
+    //       action: {
+    //         type: 'redirect',
+    //         redirect: { extensionPath: '/src/views/mindfulness-check.html' },
+    //       },
+    //       condition: {
+    //         urlFilter: 'peanuts',
+    //         resourceTypes: ['main_frame'],
+    //       },
+    //     },
+    //   ],
+    // })
+    // console.log('Checking dynamic rule count')
+    // dynamicRuleCount = await chrome.declarativeNetRequest.getDynamicRules()
+    // console.log(dynamicRuleCount)
   } catch (e) {
     console.error(e)
   }
