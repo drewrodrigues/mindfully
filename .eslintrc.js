a0a2cb3912f831b3f958892ffd05565e1a61625e
drew <drew@drews-MacBook-Pro.local>
2023-05-27 12:27:08 -0700

    Update repo w/ prettier options and packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,17 @@
 module.exports = {
+  parserOptions: {
+    sourceType: 'module',
+  },
   env: {
     browser: true,
     commonjs: true,
     es2021: true,
   },
-  extends: 'google',
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
+    semi: 0,
   },
-};
+}
