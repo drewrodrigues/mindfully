941cbd873b91eb4574996199344f22a9a9e9db84
drew <drew@drews-MacBook-Pro.local>
2023-05-27 12:29:35 -0700

    Add option script to allow additions

---
+++ src/scripts/options.js
@@ -0,0 +1,47 @@
+const { getDynamicRules, addDynamicRule } = require('./helpers/storage')
+
+const WEBSITE_MATCHER = 'website-matcher'
+
+const websiteContainer = document.querySelector("[data-id='websites']")
+const websiteAdditionForm = document.querySelector(
+  "[data-id='website-addition-form']"
+)
+const websiteMatcherInput = document.querySelector(
+  "[data-id='website-matcher-input']"
+)
+
+let rules = null
+
+websiteAdditionForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const formData = new FormData(websiteAdditionForm)
+  const ruleMatcher = formData.get(WEBSITE_MATCHER).toLocaleLowerCase()
+  const newRules = [ruleMatcher, ...rules]
+
+  chrome.storage.sync.set({ rules: newRules }).then(async () => {
+    const rule = await addDynamicRule(ruleMatcher)
+    websiteContainer.prepend(RuleBubble(rule))
+    websiteMatcherInput.value = ''
+    rules = newRules
+  })
+})
+
+async function renderWebsiteMatchesFromStorage() {
+  rules = await getDynamicRules()
+  console.log('Got rules', rules)
+
+  const elementsToAdd = []
+  for (const rule of rules) {
+    elementsToAdd.unshift(RuleBubble(rule))
+  }
+  websiteContainer.append(...elementsToAdd)
+}
+
+function RuleBubble(rule) {
+  const websiteElement = document.createElement('div')
+  websiteElement.textContent = rule.condition.urlFilter
+  websiteElement.className = 'bubble'
+  return websiteElement
+}
+
+renderWebsiteMatchesFromStorage()
