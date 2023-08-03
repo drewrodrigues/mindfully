c2e35bce4edab616dbfe046e337ed0b9b621bea0
Drew Rodrigues <rodandrew95@gmail.com>
2023-08-02 20:08:17 -0600

    More updates to signatures

--- src/scripts/services/storageService.ts
+++ src/scripts/services/storageService.ts
@@ -5,28 +5,25 @@ interface IStorageRecord {
 export abstract class StorageService<I extends IStorageRecord> {
   // public get id = UUID();
 
-  constructor() {
-    //
-  }
-
-  all(): I[] {
+  async all(): Promise<I[]> {
     // TODO:
     // * pluck from a key in the chrome storage
     return []
   }
 
-  save(record: I | I[]) {
+  async save(record: I | I[]): Promise<I> {
     // TODO
     // * before saving, assign an ID if it doesn't already have one
     // ! throw if there's an ID given
+    return {} as I
   }
 
-  update(record: I) {
+  async update(record: I): Promise<void> {
     // TODO:
     // * ignore any id that's passed, just use it to grab the record in question
   }
 
-  delete(recordId: string) {
+  async delete(recordId: string): Promise<void> {
     // TODO
   }
 }
