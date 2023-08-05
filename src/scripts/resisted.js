b246d7245e532a5514d064105d22cfedfb382e10
Drew Rodrigues <rodandrew95@gmail.com>
2023-08-05 12:57:36 -0600

    Make congrats page consistent with other pages

--- src/scripts/resisted.js
+++ src/scripts/resisted.js
@@ -4,15 +4,12 @@ const confettiCanvas = document.createElement('canvas')
 confettiCanvas.style.position = 'fixed'
 document.body.appendChild(confettiCanvas)
 
-confetti.Promise = () => {
-  console.log('confetti done')
-}
-
 const confettiAnimate = confetti.create(confettiCanvas, {
   resize: true,
   useWorker: true,
 })
 confettiAnimate({
+  colors: ['#ccff3a', '#61ff00', '#ccc'],
   particleCount: 100,
   spread: 160,
 })
